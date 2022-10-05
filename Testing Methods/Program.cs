5fba6b0b6c67c8c37ab0b9b0409006440cf29b22
Josh <thejosh2021@gmailcom>
2022-10-05 14:49:02 -0400

    initial comit

---
+++ Testing Methods/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Testing_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int num = 3;    
+            string number = num.ToString();
+
+            char[] chars = number.ToCharArray();
+            bool isNumber = char.IsDigit(number.ToCharArray()[0]);
+            
+        }
+    }
+}
